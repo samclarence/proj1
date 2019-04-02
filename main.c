12d14ded1473aba50c66d4007f5cfb85bcdb63eb
Sam Clarence <c3232490@uon.edu.au>
2019-04-02 23:49:23 +0000

    Added menu and experimented with encryption coding.

--- main.c
+++ main.c
@@ -1,6 +1,19 @@
 #include <stdio.h>
 
 int main() {
-
+    printf("Please select an option:\n");
+    printf("a) Encrypt a message using a rotation cipher and a given key.\n");
+    printf("b) Decrypt a message using a rotation cipher and a given key.\n");
+    printf("c) Encrypt a message using a substitution cipher and a given key.\n");
+    printf("d) Decrypt a message using a substitution cipher and a given key.\n");
+    
+    printf("\n"); //delete later
+    
+       
+    char c;
+    scanf("%c", &c);
+    c = c - 65;
+    printf("%d", (int)c);
+    
   return 0;
 }
