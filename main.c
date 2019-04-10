2a04bf6d12f893a4e9e792b5577f03138254f8df
Sam Clarence <c3232490@uon.edu.au>
2019-04-10 00:42:25 +0000

    Added code for encryption of a single world using the rotation cipher with a given key.

--- main.c
+++ main.c
@@ -1,24 +1,51 @@
 #include <stdio.h>
 
 int main() {
-    printf("Please select an option:\n");
+   /* printf("Please select an option:\n");
     printf("a) Encrypt a message using a rotation cipher and a given key.\n");
     printf("b) Decrypt a message using a rotation cipher and a given key.\n");
     printf("c) Encrypt a message using a substitution cipher and a given key.\n");
-    printf("d) Decrypt a message using a substitution cipher and a given key.\n");
+    printf("d) Decrypt a message using a substitution cipher and a given key.\n"); 
     
-    printf("\n"); //delete later
+    printf("\n"); //delete later */
     
-    char encryption[] = {"ABCDEFGHIJKLMNOPQRSTUVWXYZ"};
-    char a; 
-    char encA; // encryption of a
+    
+    char encryption[50];
+    char a; //the current letter to be converted
+   // char encA; // encryption of a
     int index;
-    int key = 3;
+    int key;
+        
+    // printf("Enter word to be encrypted and key: \n");
     
-    scanf("%c", &a);
-    index =  ((a - 65) + key)%26; /* Enx = (x + n)(mod26) where x = letter to be encrypted and n = key */
-    encA = encryption[index];
-    printf("The letter %c becomes %c.\n", a, encA);
+    scanf("%s", encryption); //receives the word to encrypted from 'input'
+    scanf("%d", &key); //receives the key to be used from 'input'
     
-  return 0;
+   /* a = encryption[1];
+    printf("%c\n", a); */
+    
+            
+    for(index = 0; encryption[index] != '\0' ; index++) {
+        a = encryption[index];
+        if (a >= 'A' && a <= 'Z'){
+            a =  (a + key);  //Enx = (x + n)(mod26) where x = letter to be encrypted and n = key 
+        }
+
+        else if(a >= 'a' && a <= 'z') {
+                a = (a + key);
+            }
+            printf("%c", a);
+            
+//            printf("%d\n", index); 
+            
+            // printf("%c", a);
+        }  
+
+    
+        
+            
+        
+            
+
+ return 0;
 }
