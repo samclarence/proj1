3385f91f99e0ce90bf36bfe63a71e4cd3b529172
Sam Clarence <samclarence@outlook.com>
2019-04-23 03:55:18 +0000

    Modified substitution decryption code to allow for input to contain special characters.

--- main.c
+++ main.c
@@ -11,14 +11,16 @@ int main() {
     
     
     char encryption[50];
-    char a; 
+    int a; 
     char b; 
+    char c;
     int index;
     int key;
             
         
     scanf("%[^\n]s", encryption); //receives the phrase to be encrypted from 'input'
     scanf("%d", &key); //receives the cipher key from 'input'
+
    
    /* ENCRYPTION CAESER for(index = 0; encryption[index] != '\0' ; index++) {
         a = encryption[index];
@@ -57,21 +59,23 @@ int main() {
     } */
 
 //substitution cipher
-char alphabet[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-char sub_key[26] = {'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'};
+// char alphabet[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+   char sub_key[26] =  {'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'};
 
 
 
-for(index = 0 ; encryption[index] != '\0' ; index++ ) {
+ for(index = 0 ; encryption[index] != '\0' ; index++ ) {
     a = encryption[index] - 65;
-    b = sub_key[a];
-    printf("%c", b);
-    
-    
-    
-    
-    
- }
+    if (a > 0 && a < 26.0) {
+        b = sub_key[a];
+        printf("%c", b);
+        }
+    else if (a < 0 || a > 26.0){
+        c = encryption[index];
+        printf("%c", c);
+        }
+       
+ } 
 
 
 
