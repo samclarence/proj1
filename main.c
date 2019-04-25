87626e695b5cb66e13ebebe2da7bcb4e538ab4f7
Sam Clarence <samclarence@outlook.com>
2019-04-25 10:08:21 +0000

    Polished code and made slight modifications to ensure correct operation. Added switch case to allow for user to choose which type of encryption/decryption they will be performing.

--- main.c
+++ main.c
@@ -1,39 +1,41 @@
 #include <stdio.h>
 
 int main() {
-   /* printf("Please select an option:\n");
-    printf("a) Encrypt a message using a rotation cipher and a given key.\n");
-    printf("b) Decrypt a message using a rotation cipher and a given key.\n");
-    printf("c) Encrypt a message using a substitution cipher and a given key.\n");
-    printf("d) Decrypt a message using a substitution cipher and a given key.\n"); 
-    
-    printf("\n"); //delete later */
-    
     
     char encryption[50];
     int a; 
+    char option = 'C';
+    char alphabet[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'}; //standard alphabet for conversion
+    char sub_key[26] =  {'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'}; //substitution alphabet - needs input
     int n = 0;
     int indexcount;
     char b; 
     char c;
-    char d;
     int index;
     int key;
-            
-        
+      
+    printf("Please select an option:\n");
+    printf("A) Encrypt a message using a rotation cipher and a given key.\n");
+    printf("B) Decrypt a message using a rotation cipher and a given key.\n");
+    printf("C) Encrypt a message using a substitution cipher and a given key.\n");
+    printf("D) Decrypt a message using a substitution cipher and a given key.\n"); 
+    
+    printf("\n"); //delete later */
+    
+   // scanf("%[^\n]c", &option);
     scanf("%[^\n]s", encryption); //receives the phrase to be encrypted from 'input'
     scanf("%d", &key); //receives the cipher key from 'input'
-
-   
-   /* ENCRYPTION CAESER for(index = 0; encryption[index] != '\0' ; index++) {
-        a = encryption[index];
-        if (a >= 'A' && a <= 'Z'){
-            a =  (a + key);  
+    
+    switch(option) {
+        case 'A' : printf("The encrypted message is: ");
+          for(index = 0; encryption[index] != '\0' ; index++) {
+            a = encryption[index];
+            if (a >= 'A' && a <= 'Z'){
+                a =  (a + key);  
             if(a > 'Z'){
                 a = a - 26;
-            }
-        }
-
+                }
+             }
         else if(a >= 'a' && a <= 'z') {
                 a = (a + key);
             if (a > 'z') {
@@ -42,66 +44,62 @@ int main() {
             }
             printf("%c", a);  
            
-        }  */
-         
-  /* DECRYPTION CAESER for(index = 0; encryption[index] != '\0'; index++) {
-        a = encryption[index];
-        if (a >= 'A' && a <= 'Z') {
+        }
+                    break;
+                    
+        case 'B' : printf("The decrypted message is: ");
+            for(index = 0; encryption[index] != '\0'; index++) {
+            a = encryption[index];
+              if (a >= 'A' && a <= 'Z') {
             a = (a - key);
             if (a < 'A') {
                 a = a + 26;
             }
-        }
-        else if(a >= 'a' && a <= 'z') {
+           }
+         else if(a >= 'a' && a <= 'z') {
             a = (a - key);
             if (a < 'a'){
                 a = a + 26;
             }
         }
         printf("%c", a);
-    } */
-
-//substitution cipher encryption
-   char alphabet[26] = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
-   char sub_key[26] =  {'Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M'};
-
-
-
-/* for(index = 0 ; encryption[index] != '\0' ; index++) {
-    a = encryption[index] - 65;
-    if (a >= 0 && a < 26.0) {
-        b = sub_key[a];
-        printf("%c", b);
-        }
-    else if (a < 0 || a > 26.0){
-        c = encryption[index];
-        printf("%c", c);
+    }
+                    break;
+                    
+        case 'C' : printf("The encrypted message is: ");
+          for(index = 0 ; encryption[index] != '\0' ; index++) {
+            a = encryption[index] - 65;
+             if (a >= 0 && a < 26.0) {
+                b = sub_key[a];
+                printf("%c", b);
+               }
+            else if (a < 0 || a > 26.0){
+               c = encryption[index];
+               printf("%c", c);
         }
        
- } */
-
-//substitution cipher decryption
- for(index = 0 ; encryption[index] != '\0' ; index++) {
-    b = encryption[index];
-    // printf("%c ", b);
-    for(n = 0 ; sub_key[n] != b ; n++){
-        indexcount = n;
-    } if(n >= 0 && n <= 25){
-      c = alphabet[n];
-      printf("%c", c);
-  }
-      else if(n < 0 || n > 25){
+ }
+                    break;
+                    
+        case 'D' : printf("The decrypted message is: ");
+        for(index = 0 ; encryption[index] != '\0' ; index++) {
+             b = encryption[index];
+          // printf("%c ", b);
+           for(n = 0 ; sub_key[n] != b ; n++){
+                  indexcount = n;
+           } 
+           if(n >= 0 && n <= 25){
+             c = alphabet[n];
+             printf("%c", c);
+            }
+          else if(n < 0 || n > 25){
           c = encryption[index];
           printf("%c", c);
       }
    }
-
-     
+                    break;
+        default: printf("Invalid command, please re-enter your selection.\n");
+    }
     
-    
- 
-
-
-
  return 0;
 }
